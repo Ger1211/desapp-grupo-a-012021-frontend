902601123dab4d518dc4cded50166c0e0946396e
Ger121190 <gmcabrera121190@gmail.com>
2021-05-29 17:16:01 -0300

    Agrego deuda al componente Home.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,37 +2,59 @@
   <b-container>
     <b-col align="center">
       <b-card class="mt-5">
-        <b-input-group prepend="API Key" class="mt-3">
-          <b-form-input
-            id="api-key"
-            v-model="this.$store.getters.apiKey"
-            :disabled="true"
-          ></b-form-input>
-          <b-input-group-append id="tooltip-copy">
-            <b-button
-              :variant="color"
-              v-clipboard="this.$store.getters.apiKey"
-              v-clipboard:success="copySucces"
-              v-clipboard:error="copyError"
-            >
-              <template v-if="doCopy">
-                <b-icon icon="clipboard-check"></b-icon>
-              </template>
-              <template v-else>
-                <b-icon icon="clipboard"></b-icon>
-              </template>
-            </b-button>
-          </b-input-group-append>
-        </b-input-group>
+        <b-container>
+          <b-row>
+            <b-col>
+              <b-input-group prepend="API Key" class="mt-3">
+                <b-form-input
+                  id="api-key"
+                  v-model="this.$store.getters.apiKey"
+                  :disabled="true"
+                ></b-form-input>
+                <b-input-group-append id="tooltip-copy">
+                  <b-button
+                    :variant="color"
+                    v-clipboard="this.$store.getters.apiKey"
+                    v-clipboard:success="copySucces"
+                    v-clipboard:error="copyError"
+                  >
+                    <template v-if="doCopy">
+                      <b-icon icon="clipboard-check"></b-icon>
+                    </template>
+                    <template v-else>
+                      <b-icon icon="clipboard"></b-icon>
+                    </template>
+                  </b-button>
+                </b-input-group-append>
+              </b-input-group>
 
-        <b-tooltip
-          :disabled.sync="disabled"
-          :show.sync="show"
-          ref="tooltip"
-          target="tooltip-copy"
-        >
-          Copy to clipboard!
-        </b-tooltip>
+              <b-tooltip
+                :disabled.sync="disabled"
+                :show.sync="show"
+                ref="tooltip"
+                target="tooltip-copy"
+              >
+                Copy to clipboard!
+              </b-tooltip>
+            </b-col>
+          </b-row>
+        </b-container>
+        <b-container>
+          <b-row class="mt-4">
+            <b-col>
+              <label for="resena-bill">
+                <h4>
+                  <b-badge href="#" variant="primary">Bill:</b-badge>
+                </h4>
+              </label>
+            </b-col>
+            <b-col cols="11">
+              <b-input-group size="md" prepend="$" append=".00">
+                <b-form-input id="resena-bill" class="resena-bill" :disabled="true" v-model="bill"></b-form-input>
+              </b-input-group>
+            </b-col>
+          </b-row>
+        </b-container>
       </b-card>
     </b-col>
   </b-container>
@@ -47,6 +69,7 @@ export default {
       doCopy: false,
       disabled: true,
       show: false,
+      bill: 0
     };
   },
   mounted() {
@@ -87,3 +110,8 @@ export default {
   },
 };
 </script>
+<style>
+.resena-bill { 
+    text-align: right; 
+}
+</style>
